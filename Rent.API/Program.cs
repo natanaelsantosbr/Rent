cc51fa37625a8bd9b77ecf3d79c3eb25d3df0c60
Natanael <natanaelsantosbr@gmail.com>
2024-05-05 03:56:23 -0300

    Add Serilog

--- Rent.API/Program.cs
+++ Rent.API/Program.cs
@@ -1,6 +1,16 @@
 using Rent.API.Extensions;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .Enrich.FromLogContext()
+    .CreateLogger();
+Serilog.Log.Logger = logger;
+
+builder.Host.UseSerilog(logger);
+
 builder.Services.Configurar(builder.Configuration);
 
 var app = builder.Build();
